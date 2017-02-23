82d111ce4572f8ff7b2a62b7f4d897fcce8a2849
Garett Winkler <garettwinkler@gmail.com>
2017-02-23 12:33:57 -0700

    remove debug stuff, still not 100%

--- countDistinctSlicesV2.cpp
+++ countDistinctSlicesV2.cpp
@@ -9,15 +9,13 @@
 #include <vector>       // std::vector
 
 int solution(int M, vector<int> &A) {
-    // write your code in C++14 (g++ 6.2.0)
+
     int distinctCount = 0;
     
     if (A.size() == 0) {
         return 0;   
     }
     
-    // int front = 0;
-    // int back = 0;
     int currentElem = A[0];
     int index = 0;  //index tracks where the back should be. Index must hit all indices.
     
@@ -26,10 +24,9 @@ int solution(int M, vector<int> &A) {
     for (unsigned int i = 0; i < A.size(); i++) {
         currentElem = A[i];
         int compareCount = 0;
-        
         sliceVector.push_back(currentElem);
-        
         compareCount = count(sliceVector.begin(), sliceVector.end(), currentElem);
+        
         if (compareCount == 1) {
             distinctCount++;
             if (i == A.size() - 1) {    // Occurs if we hit the end of array while on a good slice. Reset to index
@@ -42,37 +39,8 @@ int solution(int M, vector<int> &A) {
             sliceVector.clear();
             index++;
             i = index - 1;
-        }
-
-        /*
-        for (int j = back; j <= front; j++) {
-            //cout << "Comparing " << currentElem << " to " << A[j] << endl;                
-            if (currentElem == A[j]) {
-                compareCount++;   
-            }
-        }
-        
-        if (compareCount == 1) {
-            //cout << "Distinct slice here" << endl;        
-            count++;
-            front++;
-            if (i == A.size() - 1) {    // Occurs if we hit the end of array while on a good slice. Reset to index
-                index++;
-                front = index;
-                back = index;
-                i = index-1;                   
-            }
-        }
-        else {
-            //cout << "Failed slice here" << endl;
-            index++;
-            front = index;
-            back = index;
-            i = index-1;
-        }
-    */    
+        }    
     }
-    
     
     return distinctCount; 
 }
