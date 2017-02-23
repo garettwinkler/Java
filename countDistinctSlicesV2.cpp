790c2c6560da02c094081f39cc6daa783daf3ada
Garett Winkler <garettwinkler@gmail.com>
2017-02-23 12:29:40 -0700

    Create countDistinctSlicesV2.cpp
    Modified. Vector for slice makes it faster

---
+++ countDistinctSlicesV2.cpp
@@ -0,0 +1,78 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+#include <iostream>     // std::cout
+#include <algorithm>    // std::count
+#include <vector>       // std::vector
+
+int solution(int M, vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int distinctCount = 0;
+    
+    if (A.size() == 0) {
+        return 0;   
+    }
+    
+    // int front = 0;
+    // int back = 0;
+    int currentElem = A[0];
+    int index = 0;  //index tracks where the back should be. Index must hit all indices.
+    
+    vector<int> sliceVector;    //contains current range/slice
+    
+    for (unsigned int i = 0; i < A.size(); i++) {
+        currentElem = A[i];
+        int compareCount = 0;
+        
+        sliceVector.push_back(currentElem);
+        
+        compareCount = count(sliceVector.begin(), sliceVector.end(), currentElem);
+        if (compareCount == 1) {
+            distinctCount++;
+            if (i == A.size() - 1) {    // Occurs if we hit the end of array while on a good slice. Reset to index
+                sliceVector.clear();
+                index++;
+                i = index-1;                   
+            }            
+        }
+        else {
+            sliceVector.clear();
+            index++;
+            i = index - 1;
+        }
+
+        /*
+        for (int j = back; j <= front; j++) {
+            //cout << "Comparing " << currentElem << " to " << A[j] << endl;                
+            if (currentElem == A[j]) {
+                compareCount++;   
+            }
+        }
+        
+        if (compareCount == 1) {
+            //cout << "Distinct slice here" << endl;        
+            count++;
+            front++;
+            if (i == A.size() - 1) {    // Occurs if we hit the end of array while on a good slice. Reset to index
+                index++;
+                front = index;
+                back = index;
+                i = index-1;                   
+            }
+        }
+        else {
+            //cout << "Failed slice here" << endl;
+            index++;
+            front = index;
+            back = index;
+            i = index-1;
+        }
+    */    
+    }
+    
+    
+    return distinctCount; 
+}
