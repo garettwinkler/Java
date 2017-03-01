7fc23776b372db2ef90e48f3af761c2fb83048ba
Garett Winkler <garettwinkler@gmail.com>
2017-03-01 15:18:45 -0700

    Update countTriangles.cpp
    100% 25%
    71%
    Previously though sorting it would help, it doesn't.
    Still missing performance. Getting n^3, should be n^2

--- countTriangles.cpp
+++ countTriangles.cpp
@@ -12,44 +12,31 @@ int solution(vector<int> &A) {
     
     int numTri = 0;
     
-    sort(A.begin(), A.end());
+    //sort(A.begin(), A.end());
     
     int first = A[0];
     int second = A[1];
     int third = A[2];
  
-    for (unsigned int i = 0; i < A.size()-1; i++) {
+    for (unsigned int i = 0; i < A.size(); i++) {
         first = A[i];
-        second = A[i+1];
-        
-        for (unsigned int j = i+2; j < A.size(); j++) {
-            third = A[j];
-            //cout << "Checking: " << first << ", " << second << ", and " << third << endl;
-            if (first+second > third && second + third > first && first + third > second) {
-                numTri++;   
-                //cout << "the above worked ^" << endl;
-            }
-            
-        }
-        if (i == A.size()-2) {
-            for (unsigned int k = 0; k < i-1; k++) {
+        for (unsigned int j = i+1; j < A.size(); j++) {
+            second = A[j];
+            for (unsigned int k = j+1; k < A.size(); k++) {
                 third = A[k];
-                //cout << "Checking: " << first << ", " << second << ", and " << third << endl;
-                if (first+second > third && second + third > first && first + third > second) {
-                    numTri++;   
-                    //cout << "the above worked ^" << endl;
-                }                
-            }
-        }
-        
+                //cout << "Checking " << first << ", " << second << ", and " << third << endl;
+                if (first + second > third &&
+                    second + third > first &&
+                    first + third > second)     {
+                    
+                    numTri++;
+                    //cout << "the above is good" << endl;
+                }
+            }                 
+        }            
     }
     
-    // sort(A.begin(), A.end());
 
-    // for (unsigned int i = 0; i < A.size(); i++) {
-    //     cout << A[i] << " ";
-    // }
-    // cout << endl;
     
     return numTri;
 }
