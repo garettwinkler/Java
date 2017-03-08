2f0ae7edcd14262a2d828c518cbfda04fbf61965
Garett Winkler <garettwinkler@gmail.com>
2017-03-08 14:59:53 -0700

    maxCountersV2 codility
    75, 40, 55%
    For example, for the input (1, [1]) the solution returned a wrong answer (got [0] expected [1]).
    Performance too

---
+++ maxCounters.cpp
@@ -0,0 +1,25 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+vector<int> solution(int N, vector<int> &A) {
+    int size = A.size();
+    vector<int> counters;
+    counters.assign(N, 0);  //fill vector with N ints of value 0
+    
+    int currentMax = 0;
+    for (int i = 0; i < size; i++) {
+        if (A[i] < N) {
+            counters[A[i]-1]++;
+            if (counters[A[i]-1] > currentMax) {
+                currentMax = counters[A[i]-1];
+            }
+        }
+        else {
+            counters.assign(N, currentMax);
+        }
+    }
+   return counters;
+}
