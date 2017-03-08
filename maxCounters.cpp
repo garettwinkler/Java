110e101b7f4bea87faaeffcb340b0d44fbe041b8
Garett Winkler <garettwinkler@gmail.com>
2017-03-08 16:42:18 -0700

    maxCounters 100% 100%
    algo hint from comments
    So max counter operation is done after of if needed.  But bottom line it is outside the main for loop. So it's O(N+M).
    We track the min and update a counter if it's below the min and then after anything that never got upped gets brought up to the min.

--- maxCounters.cpp
+++ maxCounters.cpp
@@ -4,23 +4,39 @@
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
+#include <limits>    
+
+
 vector<int> solution(int N, vector<int> &A) {
     int size = A.size();
     vector<int> counters;
     counters.assign(N, 0);  //fill vector with N ints of value 0
     
+    int currentMin = std::numeric_limits<int>::min();   //c++ equiv of Integer.MIN_VALUE
     int currentMax = 0;
     for (int i = 0; i < size; i++) {
         if (A[i] <= N) {
+            if (counters[A[i]-1] < currentMin) {
+                counters[A[i]-1] = currentMin;    
+            }
+            
             counters[A[i]-1]++;
+            
             if (counters[A[i]-1] > currentMax) {
                 currentMax = counters[A[i]-1];
             }
         }
-        else {  //if equals N+1 (max counter operation)
-            //counters.assign(N, currentMax);
-            fill(counters.begin(), counters.end(), currentMax);
+        else {  
+            currentMin = currentMax;
         }
     }
+    
+    
+    for (int i = 0; i < N; i++) {
+        if (counters[i] < currentMin) {
+            counters[i] = currentMin;    
+        }
+    }
+    
    return counters;
 }
